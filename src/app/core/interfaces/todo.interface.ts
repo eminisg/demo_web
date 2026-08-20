b4bd94d5cad0fc25240960935f7b87bb4d5f2224
emini <emini@goldenpay.az>
2026-08-20 11:46:45 +0400

    s

--- src/app/core/interfaces/todo.interface.ts
+++ src/app/core/interfaces/todo.interface.ts
@@ -1,11 +1,21 @@
+import {TodoStatus} from '../const/enums';
 
 export interface TodoResponseInterface {
   id: number,
   title: string,
   description: string,
+  createDate: Date,
+  updateDate?: Date,
+  status: TodoStatus,
+}
+
+export interface TodoRequestInterface {
+  id?: number,
+  title: string,
+  description: string,
+  status?: TodoStatus,
 }
 
 export interface TodoPostRequestInterface {
-  title: string,
-  description: string,
+
 }
--- src/app/layout/dashboard/pages/home/home.ts
+++ src/app/layout/dashboard/pages/home/home.ts
@@ -1,9 +1,5 @@
-import {Component, inject} from '@angular/core';
-import {TodoService} from '../../../../core/todo.service';
-import {HttpClient, HttpResourceRef} from '@angular/common/http';
-import {PageableInterface} from '../../../../core/interfaces/pageable.interface';
-import {TodoPostRequestInterface, TodoResponseInterface} from '../../../../core/interfaces/todo.interface';
-import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -14,23 +10,5 @@ import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angula
   styleUrl: './home.scss',
 })
 export class Home {
-  private readonly fb = inject(FormBuilder);
-  todoService: TodoService = inject(TodoService);
-  form = this.fb.group({
-    title: new FormControl('', [Validators.required]),
-    description: new FormControl('', [Validators.required]),
-  })
 
-  todoList: HttpResourceRef<PageableInterface<TodoResponseInterface> | undefined> = this.todoService.todoList;
-
-  constructor() {
-    this.todoList.reload();
-  }
-
-  protected submit() {
-    const body = this.form.value as TodoPostRequestInterface;
-    this.todoService.createTodo(body).subscribe(()=> {
-      this.todoList.reload();
-    })
-  }
 }
