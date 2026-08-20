6f2c40b9f839e012fc141c25ff7ed6d20c445ac1
emini <emini@goldenpay.az>
2026-08-20 11:59:52 +0400

    s

--- src/app/layout/auth/auth.ts
+++ src/app/layout/auth/auth.ts
@@ -8,7 +8,7 @@ import {RouterOutlet} from '@angular/router';
   ],
   template: `
   <section class="container-sm">
-    <h1>Test 999</h1>
+    <h1>Test 999 ss</h1>
     <router-outlet></router-outlet>
   </section>
   `,
